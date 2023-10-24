df73cefae03d79d6844b5af5c59413ef113e1964
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-10-24 11:00:38 +0300

    add ToDoItem entity configuration

--- Infrastructure/Configurations/HabitConfiguration.cs
+++ Infrastructure/Configurations/HabitConfiguration.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infrastructure.Configurations;
 
-public class HabitConfiguration : IEntityTypeConfiguration<Habit>
+public sealed class HabitConfiguration : IEntityTypeConfiguration<Habit>
 {
     public void Configure(EntityTypeBuilder<Habit> builder)
     {
---
+++ Infrastructure/Configurations/ToDoItemConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Domain.ToDoItem;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+public sealed class ToDoItemConfiguration : IEntityTypeConfiguration<ToDoItem>
+{
+    public void Configure(EntityTypeBuilder<ToDoItem> builder)
+    {
+        builder.HasKey(t => t.Id);
+        builder.Property(t => t.Id)
+            .HasConversion(toDoItemId => toDoItemId.Value, value => ToDoItemId.From(value));
+        builder.ToTable(t => t.HasCheckConstraint(nameof(ToDoItem.StartTime),
+            $"{nameof(ToDoItem.StartTime)} <= {nameof(ToDoItem.EndTime)}"));
+    }
+}
