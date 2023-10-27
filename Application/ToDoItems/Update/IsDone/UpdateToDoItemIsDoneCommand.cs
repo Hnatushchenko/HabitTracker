262ece9e3e1b326df2bad546e8fa98d2d31bbd30
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-10-27 21:49:01 +0300

    update gitignore

---
+++ Application/ToDoItems/Update/IsDone/UpdateToDoItemIsDoneCommand.cs
@@ -0,0 +1,8 @@
+﻿using Domain.OneOfTypes;
+using Domain.ToDoItem;
+using MediatR;
+
+namespace Application.ToDoItems.Update;
+
+public sealed record UpdateToDoItemIsDoneCommand(ToDoItemId ToDoItemId, bool NewIsDoneValue) : IRequest<UpdatedOrNotFound>;
+public sealed record UpdateToDoItemDetailsCommand(ToDoItemId ToDoItemId, TimeOnly StartTime, TimeOnly EndTime, string Description) : IRequest<UpdatedOrNotFound>;
--- Application/ToDoItems/Update/UpdateToDoItemIsDoneCommand.cs
+++
@@ -1,7 +0,0 @@
-﻿using Domain.OneOfTypes;
-using Domain.ToDoItem;
-using MediatR;
-
-namespace Application.ToDoItems.Update;
-
-public sealed record UpdateToDoItemIsDoneCommand(ToDoItemId ToDoItemId, bool NewIsDoneValue) : IRequest<UpdatedOrNotFound>;
