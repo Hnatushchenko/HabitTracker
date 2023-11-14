46624395b246b0b0f44584f6781c38066614e20f
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-11-14 15:02:40 +0200

    enable specifying StartDate when creating new BadHabit

--- Application/BadHabits/Create/CreateBadHabitCommand.cs
+++ Application/BadHabits/Create/CreateBadHabitCommand.cs
@@ -4,5 +4,6 @@ namespace Application.BadHabits.Create;
 
 public sealed record CreateBadHabitCommand : IRequest
 {
+    public required DateTimeOffset StartDate { get; init; }
     public required string Description { get; init; }
 }
--- Application/BadHabits/Create/CreateBadHabitCommandHandler.cs
+++ Application/BadHabits/Create/CreateBadHabitCommandHandler.cs
@@ -22,7 +22,7 @@ public sealed class CreateBadHabitCommandHandler : IRequestHandler<CreateBadHabi
         {
             Id = new BadHabitId(Guid.NewGuid()),
             Description = request.Description,
-            StartDate = DateTimeOffset.UtcNow,
+            StartDate = request.StartDate,
         };
         _badHabitRepository.AddBadHabit(badHabit);
         await _unitOfWork.SaveChangesAsync(cancellationToken);
