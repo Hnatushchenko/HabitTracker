47a211d735bbec814660995e612a3e43a735cfc3
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-11-21 18:00:50 +0200

    add UpdateHabitDetailsCommandValidator

---
+++ Application/Habits/Update/UpdateHabitDetailsCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Application.Habits.Update;
+
+public sealed class UpdateHabitDetailsCommandValidator : AbstractValidator<UpdateHabitDetailsCommand>
+{
+    public UpdateHabitDetailsCommandValidator()
+    {
+        RuleFor(updateHabitDetailsCommand => updateHabitDetailsCommand.HabitId)
+            .NotEmpty();
+        RuleFor(updateHabit => updateHabit.DefaultStartTime)
+            .LessThanOrEqualTo(updateHabit => updateHabit.DefaultEndTime);
+    }
+}
