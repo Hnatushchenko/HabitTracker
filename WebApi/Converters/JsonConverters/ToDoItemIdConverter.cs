3cab1d26cc95f21720167cf3e65979e5ea189f81
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-11-27 10:12:31 +0200

    Add JsonConverter - ToDoItemIdConverter

---
+++ WebApi/Converters/JsonConverters/ToDoItemIdConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Domain.ToDoItem;
+
+namespace WebApi.Converters.JsonConverters;
+
+public sealed class ToDoItemIdConverter : JsonConverter<ToDoItemId>
+{
+    public override ToDoItemId? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType == JsonTokenType.Null)
+        {
+            return null;
+        }
+
+        var value = reader.GetGuid();
+        return new ToDoItemId(value);
+    }
+
+    public override void Write(Utf8JsonWriter writer, ToDoItemId value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.Value);
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -2,6 +2,7 @@ using System.Text.Json.Serialization;
 using Application;
 using Carter;
 using Infrastructure;
+using WebApi.Converters.JsonConverters;
 using WebApi.Extensions;
 using WebApi.Middlewares;
 
@@ -18,6 +19,7 @@ builder.Services.AddInfrastructure();
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
     options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    options.SerializerOptions.Converters.Add(new ToDoItemIdConverter());
 });
 
 builder.Services.AddCors(options =>
